a0a4ea086053ab023339a11e1cbe38bff2fc5cd8
jaymeco <jayme.anunciacao@gmail.com>
2021-05-09 13:37:35 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -35,11 +35,6 @@ export default function DonutChat() {
 
 
 
-  const mockData = {
-    series: [477138, 499928, 444867, 220426, 473088],
-    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-  }
-
   const options = {
     legend: {
       show: true
